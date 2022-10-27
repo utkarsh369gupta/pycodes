707b9269516602d16b8d44aca98a826f76e9d1ae
utkarsh gupta <utk369gupta@gmail.com>
2022-10-27 23:24:26 +0530

    added ass program

---
+++ BEFORE10pmASS.py
@@ -0,0 +1,10 @@
+# Janmansh has to submit 33 assignments for Chingari before 1010 pm and he starts to do the
+# assignments at XX pm. Each assignment takes him 11 hour to complete. Can you tell whether
+# he'll be able to complete all assignments on time or not?
+a=int(input(""))
+for i in range(a):
+    b=int(input(""))
+    if b<=7:
+        print("YES")
+    else:
+        print("NO")
